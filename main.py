28bec72c5d8b9a62f9311f5d30d94ce043365cf2
skengman1312 <dart.murtas@yahoo.it>
2022-04-22 18:27:52 +0200

    primo tentativo di simulazione

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+import sys
+sys.path.append('prova_0')
+from model import FoodModel
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
 # This is a sample Python script.
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def agent_portrayal(agent):
+    portrayal = {"Shape": "rect",
+                 "Color": "Color",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "w": 0.5,
+                 "h": 0.5}
+    if agent.type == "creature":
+        portrayal = {"Shape": "rect",
+                     "Color": "red",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "w": 0.5,
+                     "h": 0.5}
+    else:
+        portrayal = {"Shape": "circle",
+                     "Color": "green",
+                     "Filled": "true",
+                     "Layer": 1,
+                     "r": 0.2}
 
+    return portrayal
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #empty_model = FoodModel(10,10,10)
+    grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+    server = ModularServer(FoodModel,
+                          [grid],
+                          "Food Model",
+                          {"N": 5, "nf": 5, "width": 10, "height": 10})
+    server.port = 8521  # The default
+    server.launch()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- prova_0/model.py
+++ prova_0/model.py
@@ -1,18 +1,81 @@
 from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
 
 class FoodAgent(Agent):
     """An agent with fixed initial wealth."""
 
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, type):
         super().__init__(unique_id, model)
-        self.wealth = 1
+        self.hp = 5
+        self.type = type
+
+    def step(self):
+        # The agent's step will go here.
+        # For demonstration purposes we will print the agent's unique_id
+        if self.type == "creature":
+            self.move()
+            self.eat()
+            self.hp -= 1
+
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False)
+        new_position = self.random.choice(possible_steps)
+        for s in self.model.grid.get_neighbors(self.pos, moore=True, include_center=False):
+            if s.type == "food":
+                new_position = s.pos
+                break
+
+
+        self.model.grid.move_agent(self, new_position)
+
+    def eat(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        for a in cellmates:
+            if a.type == "food":
+                self.model.grid.move_to_empty(a)
+                self.hp += 5
+                break
 
 
 class FoodModel(Model):
     """A model with some number of agents."""
 
-    def __init__(self, N):
+    def __init__(self, N, nf, width, height):
         self.num_agents = N
+        self.num_food = nf
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(width, height, True)
+        self.running = True
         # Create agents
         for i in range(self.num_agents):
-            a = FoodAgent(i, self)
+            a = FoodAgent(i, self, "creature")
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+        for i in range(self.num_agents, self.num_agents + self.num_food):
+            a = FoodAgent(i, self, "food")
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+
+
+    def step(self):
+        """Advance the model by one step."""
+        self.schedule.step()
+        for a in self.schedule.agents:
+            if a.hp <= 0:
+                self.grid.remove_agent(a)
+                self.schedule.remove(a)
+
+
