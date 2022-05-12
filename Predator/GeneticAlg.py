9d58590ee1e156d166014d9d8cd450b500149e7f
edodida <edoardo.barba1999@gmail.com>
2022-05-12 15:30:43 +0200

    Modified GeneticAlg.py file, created a method to generate offspring from a given population

--- Predator/GeneticAlg.py
+++ Predator/GeneticAlg.py
@@ -1,11 +1,24 @@
 import random
+from math import modf,comb
+from itertools import combinations
 
-ind = [1,0,0,1,0,1,0,1,0,1]
+#if we take 15 individual we can make 105 groups of 2, so we can generate 210 children:
+#print(comb(15, 2))
 
 mut_rate = 0.5
+cros_rate = 0.8
 
+#create a toy population of 15 individuals
+pop = []
+for i in range(15):
+    ind = []
+    for i in range(8):
+        n = random.randint(0,1)
+        ind.append(n)
+    pop.append(ind)
 
-def crossover(cros_rate, ind1, ind2):
+#create 2 individuals from 2 parents by using uniform crossover
+def crossover(ind1, ind2, cros_rate):
     for i in range(len(ind1)):
         x = random.uniform(0,1)
         if x <= cros_rate:
@@ -16,7 +29,7 @@ def crossover(cros_rate, ind1, ind2):
     return ind1, ind2
 
 
-def mutation(mut_rate, ind):
+def mutation(ind, mut_rate):
 
     for i, n in enumerate(ind):
         x = random.uniform(0,1)
@@ -27,5 +40,16 @@ def mutation(mut_rate, ind):
                 ind[i] = 1
     return ind
 
+def gen_offspring(pop, mut_rate = 0.2, cros_rate = 0.6):
+    offspring = []
+    couples = list(combinations(pop, 2))
+    for couple in couples:
+        ind1 = couple[0]
+        ind2 = couple[1]
+        child1, child2 = crossover(ind1, ind2, cros_rate)
+        child1 = mutation(child1, mut_rate)
+        child2 = mutation(child2, mut_rate)
+        offspring.append(child1)
+        offspring.append(child2)
 
-print(mutation(mut_rate, ind))
+    return offspring
