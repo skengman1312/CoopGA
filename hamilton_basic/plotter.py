df7282683b3ac363ada77eca02558d70852954fc
vittocav <vittoria.cavicchioli@gmail.com>
2022-06-19 14:26:25 +0200

    scatter3D( ) update: added surface at 0.5

--- hamilton_basic/plotter.py
+++ hamilton_basic/plotter.py
@@ -1,43 +1,51 @@
+from matplotlib import projections
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import *
 import numpy as np
 
-data = pd.read_csv("result.csv", index_col = 0)
+data = pd.read_csv("hamilton_basic/result.csv", index_col=0)
 print(data)
+
 
 def plot_prevalence(data):
     """
     function used to plot the mean of the allele prevalence across several simulation
     """
-    data = data.drop(columns = ["N", "r", "RunId"])
-    #masking the values of each iteration
-    msk = [data["iteration"] == i for i in data["iteration"].unique()] #creating a boolean mask for each iteration to be plotted
-    ms = data["Step"].max() #max step
-    #groupling all the lines in a single df without other data
-    lines = pd.DataFrame(data = { m: data[msk[m]]["altruistic fraction"].reset_index(drop=True) for m in range(len(msk))})
-    #computing the mean
+    data = data.drop(columns=["N", "r", "RunId"])
+    # masking the values of each iteration
+    # creating a boolean mask for each iteration to be plotted
+    msk = [data["iteration"] == i for i in data["iteration"].unique()]
+    ms = data["Step"].max()  # max step
+    # groupling all the lines in a single df without other data
+    lines = pd.DataFrame(data={m: data[msk[m]]["altruistic fraction"].reset_index(
+        drop=True) for m in range(len(msk))})
+    # computing the mean
     lines['mean'] = lines.mean(axis=1)
-    #print(lines)
-    #plotting each run in thin black
-    plt.plot(lines, color = "black", lw = 0.2)
+    # print(lines)
+    # plotting each run in thin black
+    plt.plot(lines, color="black", lw=0.2)
     # plotting mean in red
-    plt.plot(lines["mean"], color = "red", lw = 0.5, label = "mean")
-    #setting plot limits
-    plt.axis((0,ms,0,1))
-    #filling the background wrt mean line
-    plt.legend(loc='best', framealpha = 0.2)
-    plt.fill_between(list(range(0,ms +1)), lines["mean"], y2=1, color ="#595FB5", alpha = 0.9 )
-    plt.fill_between(list(range(0,ms +1)), lines["mean"], color="#4DBD60", alpha=0.9)
+    plt.plot(lines["mean"], color="red", lw=0.5, label="mean")
+    # setting plot limits
+    plt.axis((0, ms, 0, 1))
+    # filling the background wrt mean line
+    plt.legend(loc='best', framealpha=0.2)
+    plt.fill_between(list(range(0, ms + 1)),
+                     lines["mean"], y2=1, color="#595FB5", alpha=0.9)
+    plt.fill_between(list(range(0, ms + 1)),
+                     lines["mean"], color="#4DBD60", alpha=0.9)
     for spine in plt.gca().spines.values():
         spine.set_visible(False)
     plt.xlabel("Steps")
     plt.ylabel("Population fraction")
 
-
     plt.show()
     pass
+
+
+def f(x, y, n):
+    return np.full_like(np.zeros(100), 0.5, shape=n)
 
 
 def scatter3D(data, param1, param2, result, labels):
@@ -49,31 +57,12 @@ def scatter3D(data, param1, param2, result, labels):
 
     # initializing the plot
     fig = plt.figure()
-    #ax = plt.axes(projection='3d')
-    ax = fig.add_subplot(111, projection='3d')
+    ax = plt.axes(projection='3d')
 
     # setting the labels
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel(labels[2])
-
-    '''
-    FOR THE FUCKING PLANE, TO BE DONE
-    # a plane is a*x+b*y+c*z+d=0
-    #
-    # [a,b,c] is the normal
-    normal = np.array([0, 0, 0.5])
-    #point = np.array([1, 2, 3])
-    #d = -point*normal
-    # create x,y
-    xx, yy = np.meshgrid(range(10), range(10))
-    # calculate corresponding z
-    z = np.full_like(np.zeros(100), 0.5, shape=(10, 10))
-    # z.fill(0.5)
-    print(type(z), z)
-    # plot the surface
-    ax.plot_surface(xx, yy, z, alpha=0.2)
-    '''
 
     # we obtain the unique labels of the RunId
     # In this case I'm considering each label of RunId correspond to a specific
@@ -84,14 +73,26 @@ def scatter3D(data, param1, param2, result, labels):
     for run in runs:
         # for each combination of parameters we will have a different color
         run_data = results[results["RunId"] == run]
-        ax.scatter3D(run_data[param1], run_data[param2],
-                     run_data[result])
+        ax.scatter(run_data[param1], run_data[param2], run_data[result])
+
+    if results[result].min() > 0.5:
+        ax.set_zlim(0.3, 1)
+
+    # plot the surface
+    m = results[param1].min()
+    n = results[param2].min()
+    xx, yy = np.meshgrid(np.arange((m-0.5), (m+0.5), 0.1),
+                         np.arange((n-0.5), (n+0.5), 0.1))
+
+    z = f(xx, yy, xx.shape)
+
+    ax.plot_surface(xx, yy, z, alpha=0.2)
 
     plt.show()
 
 
 labels = ["population size",
           "initial freq altruism", "ending freq altruism"]
-#scatter3D(data, "N", "r", "altruistic fraction", labels)
+scatter3D(data, "N", "r", "altruistic fraction", labels)
 
-plot_prevalence(data)
+# plot_prevalence(data)
