fa4492253778d73ff186302c7ea2749529d33d81
rossana24 <rossana.cervesato@hotmail.it>
2022-06-22 17:53:01 +0200

    plot_all_agents() to create a plot of agents with different genotypes

--- green_beard_advanced/plotter.py
+++ green_beard_advanced/plotter.py
@@ -74,6 +74,69 @@ def multi_plot_prevalence(data, params=["N", "r", "dr", "mr", "cr"]):
             drop=True), title=f"Run {data[m]['pID'].max() + 1}", params=params)
 
 
+def scatter3D(data, param1, param2, result, labels, all_params, title=""):
+    """
+    Function used to plot altruistic allele frequency (result) against two other parameters of user's choice.
+    In the output each color will represent a different combination of parameters' values
+    """
+
+    data = get_all_param_ID(data, all_params)
+
+    # we want to plot just the value of the parameters in the last step of each iteration
+    max_step = data["Step"].max()
+    # results stores now just the lines of the df where we had the last step of each iteration
+    results = data[data["Step"] == max_step]
+
+    # initializing the plot
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    # setting the labels
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    ax.set_zlabel(labels[2])
+
+    # we obtain the unique labels of the pID
+    # Each label of RunId correspond to a specific combination of values for the paramters
+    runs = results["pID"].unique()
+
+    handles = []
+
+    for run in runs:
+        # for each combination of parameters we will have a different color
+        run_data = results[results["pID"] == run]
+        prova = ax.scatter(
+            run_data[param1], run_data[param2], run_data[result])
+        handles.append(prova)
+
+    if results[result].min() > 0.5:
+        ax.set_zlim(0.3, 1)
+
+    # adjusting the axes limits
+    p1_min = results[param1].min()-results[param1].min() * 20/100
+    p1_max = results[param1].max()+results[param1].min() * 20/100
+    p2_min = results[param2].min()-results[param2].min() * 20/100
+    p2_max = results[param2].max()+results[param2].min() * 20/100
+
+    ax.set_xlim(p1_min, p1_max)
+    ax.set_ylim(p2_min, p2_max)
+
+    # plot the surface
+    xx, yy = np.meshgrid(np.arange(p1_min, p1_max, (p1_max-p1_min)/100),
+                         np.arange(p2_min, p2_max, (p1_max-p1_min)/100))
+    z = np.full_like(np.zeros(100), 0.5, shape=xx.shape)
+
+    ax.plot_surface(xx, yy, z, alpha=0.2)
+
+    ax.legend(handles, runs.data)
+
+    maintitle = f"Kinship altruism {title}" if title else "Kinship altruism"
+    plt.title(f"{maintitle}")
+    filename = f"{title.replace(' ', '')}_results.png" if title else "results.png"
+    plt.savefig(filename)
+
+    plt.show()
+
 
 # ----- FOR 4 ALLELES ------
 
@@ -176,7 +239,6 @@ def plot_all_prevalence(data, title="", params=["N", "r", "dr", "mr", "cr"]):
     plt.savefig(filename)
     plt.show()
 
-
     pass
 
 
@@ -201,70 +263,6 @@ def multi_plot_all_prevalence(data, params=["N", "r", "dr", "mr", "cr"], sub="")
     for m in msk:
         plot_all_prevalence(data[m].reset_index(
             drop=True), title=f"Run {data[m]['pID'].max() + 1}: " + sub, params=params)
-
-
-def scatter3D(data, param1, param2, result, labels, all_params, title=""):
-    """
-    Function used to plot altruistic allele frequency (result) against two other parameters of user's choice.
-    In the output each color will represent a different combination of parameters' values
-    """
-
-    data = get_all_param_ID(data, all_params)
-
-    # we want to plot just the value of the parameters in the last step of each iteration
-    max_step = data["Step"].max()
-    # results stores now just the lines of the df where we had the last step of each iteration
-    results = data[data["Step"] == max_step]
-
-    # initializing the plot
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    # setting the labels
-    ax.set_xlabel(labels[0])
-    ax.set_ylabel(labels[1])
-    ax.set_zlabel(labels[2])
-
-    # we obtain the unique labels of the pID
-    # Each label of RunId correspond to a specific combination of values for the paramters
-    runs = results["pID"].unique()
-
-    handles = []
-
-    for run in runs:
-        # for each combination of parameters we will have a different color
-        run_data = results[results["pID"] == run]
-        prova = ax.scatter(
-            run_data[param1], run_data[param2], run_data[result])
-        handles.append(prova)
-
-    if results[result].min() > 0.5:
-        ax.set_zlim(0.3, 1)
-
-    # adjusting the axes limits
-    p1_min = results[param1].min()-results[param1].min() * 20/100
-    p1_max = results[param1].max()+results[param1].min() * 20/100
-    p2_min = results[param2].min()-results[param2].min() * 20/100
-    p2_max = results[param2].max()+results[param2].min() * 20/100
-
-    ax.set_xlim(p1_min, p1_max)
-    ax.set_ylim(p2_min, p2_max)
-
-    # plot the surface
-    xx, yy = np.meshgrid(np.arange(p1_min, p1_max, (p1_max-p1_min)/100),
-                         np.arange(p2_min, p2_max, (p1_max-p1_min)/100))
-    z = np.full_like(np.zeros(100), 0.5, shape=xx.shape)
-
-    ax.plot_surface(xx, yy, z, alpha=0.2)
-
-    ax.legend(handles, runs.data)
-
-    maintitle = f"Kinship altruism {title}" if title else "Kinship altruism"
-    plt.title(f"{maintitle}")
-    filename = f"{title.replace(' ', '')}_results.png" if title else "results.png"
-    plt.savefig(filename)
-
-    plt.show()
 
 
 def scatter3D_all(data, param1, param2, result, labels, all_params, title=""):
@@ -330,17 +328,84 @@ def scatter3D_all(data, param1, param2, result, labels, all_params, title=""):
 
     plt.show()
 
+
+def plot_all_agents(data, title="", params=["N", "r", "dr", "mr", "cr"]):
+    """
+    function used to plot the number of agents with different genotypes across several simulation
+    """
+    # data = data.drop(columns=["N", "r", "RunId"])
+    # masking the values of each iteration
+    # creating a boolean mask for each iteration to be plotted
+
+    msk = [data["iteration"] == i for i in data["iteration"].unique()]
+    ms = data["Step"].max()  # max step  printa step r
+
+    # grouping all the lines in a single df without other data
+    lines_true = pd.DataFrame(data={m: (data[msk[m]]["true beards fraction"]*data[msk[m]]["n_agents"]).reset_index(
+        drop=True) for m in range(len(msk))})
+    lines_suckers = pd.DataFrame(data={m: (data[msk[m]]["suckers fraction"]*data[msk[m]]["n_agents"]).reset_index(
+        drop=True) for m in range(len(msk))})
+    lines_impostors = pd.DataFrame(data={m: (data[msk[m]]["impostors fraction"]*data[msk[m]]["n_agents"]).reset_index(
+        drop=True) for m in range(len(msk))})
+    lines_cowards = pd.DataFrame(data={m: (data[msk[m]]["cowards fraction"]*data[msk[m]]["n_agents"]).reset_index(
+        drop=True) for m in range(len(msk))})
+
+    # computing the mean
+    lines_true['mean'] = lines_true.mean(axis=1)
+    lines_suckers['mean'] = lines_suckers.mean(axis=1)
+    lines_impostors['mean'] = lines_impostors.mean(axis=1)
+    lines_cowards['mean'] = lines_cowards.mean(axis=1)
+
+    # setting plot limits
+    max_agents = max(data["n_agents"])
+    print(max_agents)
+    plt.axis((0, ms, 0, max_agents))
+
+    # plot lines
+    plt.plot(lines_true['mean'], color="#120A8F", lw=0.5)
+    plt.plot(lines_suckers['mean'], color="#FF1493", lw=0.5)
+    plt.plot(lines_cowards['mean'], color="#B8860B", lw=0.5)
+    plt.plot(lines_impostors['mean'], color="#006400", lw=0.5)
+
+    plt.xlabel("Steps")
+    plt.ylabel("Number of individuals")
+    maintitle = f"{title}" if title else "Green Beard"
+    subtitle = " ".join([f"{p}={data[p][0]}" for p in params])
+
+    plt.title(f"{maintitle}\n{subtitle}")
+    plt.legend(["true beards", "suckers", "cowards", "impostors"], loc='best', framealpha=0.2, title="Labels")
+    filename = f"{title.replace(' ', '')}_results.png" if title else "results.png"
+    plt.savefig(filename)
+    plt.show()
+
+    pass
+
+
 if __name__ == "__main__":
 
-    # ----- FOR ALTRUISTIC FRACTION ------
+    #### ----- FOR ALTRUISTIC FRACTION ------
 
     #plot_prevalence(data, title="Green Beard linkage disequilibrium")
     #plot_prevalence(data1, title="Green Beard")
 
+    plot_all_agents(data, title="Green Beard linkage disequilibrium")
+    #plot_all_agents(data1, title="Green Beard")
+
     #multi_plot_prevalence(multidata, sub="Green Beard linkage disequilibrium")
     #multi_plot_prevalence(multidata1, sub="Green Beard")
 
-    # ----- FOR 4 ALLELES ------
+    labels = ["mutation rate", "death rate", "ending freq altruism"]
+    #scatter3D(data, param1="mr", param2="dr", result="altruistic fraction", labels=labels,
+        #all_params=["N", "r", "dr", "mr"], title="scatter")
+    #scatter3D(data1, param1="mr", param2="dr", result="altruistic fraction", labels=labels,
+        #all_params=["N", "r", "dr", "mr"], title="scatter")
+
+    #scatter3D(multidata, param1="mr", param2="dr", result="altruistic fraction", labels=labels,
+          #all_params=["N", "r", "dr", "mr"], title="scatter")
+    #scatter3D(multidata1, param1="mr", param2="dr", result="altruistic fraction", labels=labels,
+          #all_params=["N", "r", "dr", "mr"], title="scatter")
+
+    #### ----- FOR 4 ALLELES ------
 
     #plot_all_prevalence(data, title="Green Beard linkage disequilibrium")
     #plot_all_prevalence(data1, title="Green Beard")
