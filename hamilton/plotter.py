0fa54515649a43effa70809cab260fac67cd1bd0
Pietro <dart.murtas@yahoo.it>
2022-06-26 12:43:09 +0200

    implemented fitness graph tofix: fitness update

--- hamilton/plotter.py
+++ hamilton/plotter.py
@@ -48,8 +48,6 @@ def plot_prevalence(data, title="", params=["N", "r", "dr", "mr"]):
     filename = f"{title.replace(' ', '')}_results.png" if title else "results.png"
     plt.savefig(filename)
     plt.show()
-    pass
-
 
 def get_param_ID(data, params=["N", "r", "dr", "mr"]):
     """
@@ -63,7 +61,6 @@ def get_param_ID(data, params=["N", "r", "dr", "mr"]):
     data["pID"] = data["pID"].apply(lambda x: mapdict[x])
     return data
 
-
 def multi_plot_prevalence(data, params=["N", "r", "dr", "mr"]):
     """
     function used to plot the mean of the allele prevalence across several simulation and with different hyperparameters
@@ -76,6 +73,31 @@ def multi_plot_prevalence(data, params=["N", "r", "dr", "mr"]):
     for m in msk:
         plot_prevalence(data[m].reset_index(
             drop=True), title=f"Run {data[m]['pID'].max() + 1}", params=params)
+
+def plot_rep_fitness(data, s = 100, title = "", params=["N", "r", "dr", "mr"]):
+    msk = [data["iteration"] == i for i in data["iteration"].unique()]
+    ms = data["Step"].max()
+    finallines = {}
+    for c in ["mean rep", "min rep", "max rep", "mean rep0", "mean rep1"]:
+        lines = pd.DataFrame(data={m: data[msk[m]][c].reset_index(
+            drop=True) for m in range(len(msk))})
+        finallines[c] = lines.mean(axis=1)
+    plt.plot(finallines["mean rep"], color="black", lw=1, label = "Overall mean")
+    plt.plot(finallines["min rep"], color = "red", lw = 0.5, label = "Minimum")
+    plt.plot(finallines["max rep"], color = "magenta", lw = 0.5, label = "Maximum")
+    plt.plot(finallines["mean rep0"], color= "#4DBD60", lw=0.5, label = "Altruistic mean")
+    plt.plot(finallines["mean rep1"], color= "#595FB5", lw=0.5, label = "Egoistic mean")
+    plt.legend(loc='lower center', framealpha=0.8, bbox_to_anchor=(0.5, -0.3), ncol = 3, fancybox=True, prop={'size': 8})
+    plt.axis((0, ms, -0.1, 1.1))
+    maintitle = f"Secondary fitness {title}" if title else "Secondary fitness"
+    subtitle = " ".join([f"{p}={data[p][0]}" for p in params])
+    plt.title(f"{maintitle}\n{subtitle}")
+    filename = f"{title.replace(' ', '')}_fitness_results.png" if title else "fitness_results.png"
+    for spine in plt.gca().spines.values():
+        spine.set_visible(False)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.show()
 
 
 def scatter3D(data, param1, param2, result, labels, all_params, title=""):
@@ -147,4 +169,5 @@ labels = ["mutation rate", "death rate", "ending freq altruism"]
 #          all_params=["N", "r", "dr", "mr"], title="scatter")
 
 #multi_plot_prevalence(multidata)
-plot_prevalence(multidata)
+#plot_prevalence(multidata)
+plot_rep_fitness(data)
