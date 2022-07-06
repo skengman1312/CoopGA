2c7da9338504e72c7ff0a9be58110cfe61a9643d
rossana24 <rossana.cervesato@hotmail.it>
2022-07-06 11:56:23 +0200

    Additional_scenarios directory (I)

---
+++ Additional_scenarios/prova_0/main.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('prova_0')
+from model import FoodModel
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
+from mesa.visualization.modules import ChartModule
+
+#prima prova di simulazione con visualzazione su server in tempo reale
+
+#regole molto semplici:
+# 5hp di partenza; -1hp a step; +5hp per unità di cibo consumata
+# movimento di una casella a step per ogni creatura;
+# se c'è cibo nelle vicinanze il movimento sarà verso il cibo, altrimenti sarà randomico.
+# se gli hp scendono a 0 la creatura è eliminata
+# il numero di cibo è costante
+
+# prova
+
+def agent_portrayal(agent):
+    if agent.type == "creature":
+        portrayal = {"Shape": "rect",
+                     "Color": "blue",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "w": 0.5,
+                     "h": 0.5}
+    else:
+        portrayal = {"Shape": "circle",
+                     "Color": "green",
+                     "Filled": "true",
+                     "Layer": 1,
+                     "r": 0.2}
+
+    return portrayal
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #empty_model = FoodModel(10,10,10)
+    grid = CanvasGrid(agent_portrayal, 100, 100, 500, 500)
+    chart_0 = ChartModule([{"Label": "Mean food","Color": "Black"}], data_collector_name='datacollector', canvas_height=100, canvas_width=200)
+    chart_1 = ChartModule([{"Label": "Number of creatures", "Color": "Blue"}], data_collector_name='datacollector',
+                        canvas_height=100, canvas_width=200)
+    server = ModularServer(FoodModel,
+                          [grid, chart_0, chart_1],
+                          "Food Model",
+                          {"N": UserSettableParameter("slider", "Creature number", 5, 0, 200, 1), "nf": UserSettableParameter("slider", "Food number", 5, 0, 200, 1),
+                           "sight" : UserSettableParameter("slider", "Creature sight", 5, 0, 20, 1),
+                           "width": 100, "height": 100})
+    server.port = 8521  # The default
+    server.launch()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
